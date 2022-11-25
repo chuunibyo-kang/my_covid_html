bc585293fd77d658a97825bff795126580d8a474
chuunibyo-kang <a793239433@Gmail.com>
2022-11-26 01:46:18 +0800

    update

--- app_run.py
+++ app_run.py
@@ -11,7 +11,6 @@ from map_build import *
 from get_data import *
 from crawler import *
 
-#获取IP
 app = Flask(__name__, static_folder = "static",template_folder = "templates")
 
 #获取本土数据，把数据类型转换成列表类型
@@ -32,13 +31,12 @@ def pass_whole_data():
             whole_data[i] = "+"+str(whole_data[i])
     return whole_data
 
-#更新日期
+#服务器更新日期
 def update_date():
     date = get_data_update_date()
     return date
 
-#路由部分，多数用于返回疫情数据
-
+#设置首页路由，传入的数据包括本土数据、整体数据、服务器更新数据时间
 @app.route("/")
 def index():
     return render_template(
@@ -55,42 +53,103 @@ def click_update_data():
     crawler_run()
     return "更新成功"
 
+#设置整体疫情数据表网页路由，传入的数据包括本土数据、整体数据、服务器更新数据时间
+@app.route("/covid_information_table")
+def covid_information_table():
+    return render_template(
+            'covid_information_table.html',
+            mainland_data = pass_mainland_data(),
+            whole_data = pass_whole_data(),
+            update_date = update_date(),
+            )
 
-# 初始化可视化地图，并将图的数据转化为json，用于html页面调用echarts再次生成
-# 这样做能让也面生成更灵活
-@app.route("/visual_map")
-def visual_map():
+
+#设置可视化疫情数据地图数据路由
+#这个路由传输的是数据，用于网页内的ajax生成图表数据的请求
+@app.route("/visual_China_map_data")
+def visual_map_data():
     map = visual_data_map()
     return map.dump_options_with_quotes()
+#设置可视化疫情数据地图网页路由
+@app.route("/visual_China_map")
+def visual_map():
+    return render_template(
+            'visual_China_map.html',
+            mainland_data = pass_mainland_data(),
+            whole_data = pass_whole_data(),
+            update_date = update_date(),
+            )
 
-# 初始化近期整体趋势折线图，并将图的数据转化为json，用于html页面调用echarts再次生成
-@app.route("/current_overall_line_map")
-def recent_overall_line_map():
+#设置近期整体趋势折线图json数据路由
+#这个路由传输的是数据，用于网页内的ajax生成图表数据的请求
+@app.route("/current_overall_line_map_data")
+def current_overall_line_map_data_data():
     overall_line_map =  recent_overall_data_line_map()
     return overall_line_map.dump_options_with_quotes()
+#设置近期整体趋势折线图网页路由
+@app.route("/current_overall_line_map")
+def rcurrent_overall_line_map():
+    return render_template(
+            'current_overall_line_map.html',
+            mainland_data = pass_mainland_data(),
+            whole_data = pass_whole_data(),
+            update_date = update_date(),
+            )
 
-# 初始化近期新增数据折线图，并将图的数据转化为json，用于html页面调用echarts再次生成
-@app.route("/current_daily_line_map")
-def recent_daily_line_map():
+
+#设置近期新增数据折线图json数据路由
+#这个路由传输的是数据，用于网页内的ajax生成图表数据的请求
+@app.route("/current_daily_line_map_data")
+def current_daily_line_map_data():
     daily_line_map = recent_daily_data_line_map()
     return daily_line_map.dump_options_with_quotes()
+#设置近期新增数据折线图网页路由
+@app.route("/current_daily_line_map")
+def current_daily_line_map():
+    return render_template(
+            'current_daily_line_map.html',
+            mainland_data = pass_mainland_data(),
+            whole_data = pass_whole_data(),
+            update_date = update_date(),
+            )
 
-# 初始化累计确诊TOP5柱状图，并将图的数据转化为json，用于html页面调用echarts再次生成
-@app.route("/total_confirm_top5_data_map")
-def total_confirm_top5_map():
+#设置累计确诊TOP5柱状图数据json数据路由
+#这个路由传输的是数据，用于网页内的ajax生成图表数据的请求
+@app.route("/total_confirm_top5_data_map_data")
+def total_confirm_top5_map_data():
     total_bar_map = total_confirm_top5_data_map()
     return total_bar_map.dump_options_with_quotes()
+#设置累计确诊TOP5柱状图网页路由
+@app.route("/total_confirm_top5_data_map")
+def total_confirm_top5_map():
+    return render_template(
+            'total_confirm_top5_data_map.html',
+            mainland_data = pass_mainland_data(),
+            whole_data = pass_whole_data(),
+            update_date = update_date(),
+            )
 
-# 初始化新增确诊TOP5柱状图，并将图的数据转化为json，用于html页面调用echarts再次生成
-@app.route("/today_confirm_add_top5_data_map")
-def today_confirm_add_top5_map():
+#设置新增确诊TOP5柱状图数据json数据路由
+#这个路由传输的是数据，用于网页内的ajax生成图表数据的请求
+@app.route("/today_confirm_add_top5_data_map_data")
+def today_confirm_add_top5_map_data():
     today_bar_map = today_confirm_add_top5_data_map()
     return today_bar_map.dump_options_with_quotes()
+#设置新增确诊TOP5柱状图网页路由
+@app.route("/today_confirm_add_top5_data_map")
+def today_confirm_add_top5_map():
+    return render_template(
+            'today_confirm_add_top5_data_map.html',
+            mainland_data = pass_mainland_data(),
+            whole_data = pass_whole_data(),
+            update_date = update_date(),
+            )
 
 if __name__ == "__main__":
     #如果想单独看网页数据，可以用build_charts_html的方法
     # build_charts_html()
     app.run(
+        #这里的IP最好是设置成本机的IP
             host="0.0.0.0"
             ,port = PORT
             ,debug = True
